b7d6e9868803ade9699658961a312dd4b83114bf
Prof.Dragon <kenoog@freenet.de>
2018-09-19 07:28:38 +0200

    Update ssh-status.1min.sh

--- ssh-status.1min.sh
+++ ssh-status.1min.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
+# <bitbar.title>SSH Status</bitbar.title>
+# <bitbar.version>v0.2.0</bitbar.version>
+# <bitbar.author>Keno Oelrichs Garcia</bitbar.author>
+# <bitbar.author.github>MrDrache333</bitbar.author.github>
+# <bitbar.image>http://i.imgur.com/s8FMCLG.png</bitbar.image>
+# <bitbar.desc>Continuously checks if hosts are available for ssh connection</bitbar.desc>
+# <bitbar.dependencies>Bash</bitbar.dependencies>
+#
+# SSH Status plugin
+# by Keno Oelrichs Garcia (@Kenoog)
+# THIS PROGRAM IN ITS FIRST VERSION WAS ORIGINALLY WRITTEN BY @oliviernt
+#
+# Continuously checks if hosts are available for ssh connection on port 22
 #HOSTS are the HOSTS you want to look for
 HOSTS=("raspberrypi.local" "example.com")
 #USERS are the Usernames to login with if you klick on the Host
